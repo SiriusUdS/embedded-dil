99d5240b86001bed54651e49f4d918fc4f181253
Laurent Breton <laurent.breton03@gmail.com>
2025-11-21 20:04:40 -0500

    cmakelist et control led

---
+++ Control/Inc/led_flasher.h
@@ -0,0 +1,8 @@
+#ifndef LED_FLASHER_H
+#define LED_FLASHER_H
+
+#include "led.h"
+
+void flash_led_run(led_t led);
+
+#endif
---
+++ Control/Src/led_flasher.c
@@ -0,0 +1,8 @@
+#include "led_flasher.h"
+
+void led_flasher_run(led_t led)
+{
+    // faire delay ou if timer expired
+
+    led_toggle(led);
+}
